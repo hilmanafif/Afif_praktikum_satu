f0363523900dd19a221e3bd5d1117c837feb6965
root <diki.andeas@jerbeeindonesia.com>
2017-10-17 05:08:20 +0000

    Testing kalkulasi

--- app/Http/Controllers/PayrollsController.php
+++ app/Http/Controllers/PayrollsController.php
@@ -107,9 +107,9 @@ class PayrollsController extends Controller
           $subtotalb = $employee->jabatans->Tunpres + $employee->jabatans->Tunjab + $employee->tunjanganPelaksana($employee->tupel_id) + $employee->jabatans->Tunken;
           $totalpenghasilan = $subtotala + $subtotalb;
           $totalpotongan = 0;
-          /*
+
           $totalpotongan = $employee->potongan->bpjs + $employee->potongan->dapenma + $employee->potongan->bpjskes + $employee->potongan->bpjspensiun + $employee->potongan->zakat + $employee->potongan->bjb + $employee->potongan->iurandw + $employee->potongan->tabungan + $employee->potongan->warung + $employee->potongan->pinjrutin + $employee->potongan->pinjperum + $employee->potongan->utangpeg + $employee->potongan->potlain + $employee->potongan->iuranykpp;
-          */
+          
           $totalgajibersih = $totalpenghasilan - $totalpotongan;
 
           $datas = ["title" => $slip_name,
